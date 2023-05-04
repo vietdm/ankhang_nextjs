dd404d1187f6000bd078b002a65e0985197076e4
Do Thi Thuy <57499079+thuysddoox@users.noreply.github.com>
2023-05-04 14:12:03 +0000

    Update 6 files

---
+++ pages/user/index.tsx
@@ -0,0 +1,14 @@
+import Layout from "@/components/layout";
+import UserComponent from "@/components/user/User";
+
+const ProfilePage = ()=>{
+  return (
+    <Layout>
+      <>
+        <UserComponent />
+      </>
+    </Layout>
+  )
+}
+
+export default ProfilePage;
