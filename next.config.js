1721de02fa35b9f3da1a9c5ef3c1703e4909c6a3
vietdau33 <vietdau33@gmail.com>
2023-05-09 01:13:16 +0700

    a

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: [
-      "api.ankhang.asia"
-    ],
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "**",
-      },
-    ],
-  },
   typescript: {
     ignoreBuildErrors: true,
   }
