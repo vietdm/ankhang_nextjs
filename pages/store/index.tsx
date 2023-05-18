d053647283d8ee08716bc839142812f3d1456979
vietdau33 <vietdau33@gmail.com>
2023-05-19 05:33:17 +0700

    a

--- pages/store/index.tsx
+++ pages/store/index.tsx
@@ -1,11 +1,21 @@
 import { fetch } from "@/libraries/axios";
-import { Stack } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import { SinglePage } from "@/components/ui/SinglePage";
 import { BoxProductSimple } from "@/components/store/BoxProductSimple";
 
 const StorePage = () => {
     const [products, setProducts] = useState<any>([]);
+    const [productShow, setProductShow] = useState<any>([]);
+    const [menuActive, setMenuActive] = useState<string>('sp');
+
+    const filterProduct = () => {
+        const productActive = products.filter((product: any) => {
+            if (menuActive == 'sp') return product.price < 3000000;
+            return product.price >= 3000000;
+        });
+        setProductShow(productActive);
+    }
 
     useEffect(() => {
         fetch.get('/products').then((result: any) => {
@@ -13,10 +23,26 @@ const StorePage = () => {
         });
     }, []);
 
+    useEffect(() => {
+        filterProduct();
+    }, [products, menuActive]);
+
     return (
         <SinglePage title="Cửa hàng">
+            <Stack direction="row" paddingY="20px">
+                <Box width="50%" paddingY="10px" onClick={() => setMenuActive('sp')} sx={{
+                    borderBottom: menuActive == 'sp' ? "3px solid green" : null
+                }}>
+                    <Typography component="h4" textAlign="center">SẢN PHẨM</Typography>
+                </Box>
+                <Box width="50%" paddingY="10px" onClick={() => setMenuActive('combo')} sx={{
+                    borderBottom: menuActive == 'combo' ? "3px solid green" : null
+                }}>
+                    <Typography component="h4" textAlign="center">COMBO</Typography>
+                </Box>
+            </Stack>
             <Stack direction="row" flexWrap="wrap" overflow="auto" marginTop={0}>
-                {products.map((product: any) => <BoxProductSimple product={product} key={product.id} />)}
+                {productShow.map((product: any) => <BoxProductSimple product={product} key={product.id} />)}
             </Stack>
         </SinglePage>
     );
