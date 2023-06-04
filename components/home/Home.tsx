e46f6a5bcaacdb1ff5dddc808f0f29c807b64297
vietdau33 <vietdau33@gmail.com>
2023-06-04 13:52:45 +0700

    save

--- components/home/Home.tsx
+++ components/home/Home.tsx
@@ -235,14 +235,6 @@ export const HomeComponent = ({ active = false }: { active?: boolean }) => {
                     <Typography color="#0049a5" fontWeight="700" component="p" fontSize={16} textAlign="right">{formatMoney(user?.reward_point)}</Typography>
                 </BoxMenu>
                 <BoxMenu>
-                    <Typography color="#0049a5" fontWeight="700" component="h4" fontSize={17}>Chức vụ</Typography>
-                    <Typography color="#0049a5" fontWeight="700" component="p" fontSize={16} textAlign="right">{userLevel(user?.level)}</Typography>
-                </BoxMenu>
-                <BoxMenu>
-                    <Typography color="#0049a5" fontWeight="700" component="h4" fontSize={17}>Tổng doanh số</Typography>
-                    <Typography color="#0049a5" fontWeight="700" component="p" fontSize={16} textAlign="right">{formatMoney(dashboardData?.total_sale ?? 0)}</Typography>
-                </BoxMenu>
-                <BoxMenu>
                     <Typography color="#0049a5" fontWeight="700" component="h4" fontSize={17}>Tổng hoa hồng</Typography>
                     <Typography color="#0049a5" fontWeight="700" component="p" fontSize={16} textAlign="right">{formatMoney(dashboardData?.money_bonus ?? 0)}</Typography>
                 </BoxMenu>
