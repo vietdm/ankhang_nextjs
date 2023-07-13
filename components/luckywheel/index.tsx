5b48878be9f621c6f39e3ca65366a2a9a62511bf
vietdm <vietdau33@gmail.com>
2023-07-14 01:03:00 +0700

    save

--- components/luckywheel/index.tsx
+++ components/luckywheel/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, Stack, Typography } from "@mui/material";
 import { CallSupport } from "../ui/CallSupport";
 import { DialogQc } from "../home/DialogQc";
 
@@ -13,36 +13,31 @@ export const LuckyWheel = ({ active = false }: { active?: boolean }) => {
 
   return (
     <Box display={active ? "block" : "none"}>
-      <Box sx={{ fontSize: '18px', padding: '15px' }}>
+      <Box sx={{ fontSize: '18px', padding: '15px', paddingTop: 5 }}>
         📢🔥 <b>&quot;Thịnh vượng cùng An Khang&quot;</b> - Chương trình khuyến mãi lớn đang đổ bộ!
         <br />
         <br />
-        🌟🎉 Bạn đã sẵn sàng cho sự kiện thịnh vượng lớn? Hãy cùng <b>An Khang</b> trải nghiệm cảm giác <b>&quot;Mua là có - Quay là trúng&quot;</b>!
+        🌟🎉 Bạn đã sẵn sàng cho sự kiện thịnh vượng lớn? Hãy cùng An Khang trải nghiệm cảm giác <b>&quot;Mua là có - Quay là trúng&quot;</b>!
         <br />
         <br />
-        🎁🎉 Chương trình khuyến mãi đặc biệt này dành cho tất cả khách hàng, bất kể bạn muốn mua bất cứ sản phẩm nào từ <b>An Khang</b>. Chỉ cần đặt hàng từ 1 sản phẩm trở lên, bạn sẽ có cơ hội tham gia quay trúng thưởng và nhận ngay những phần quà vô cùng hấp dẫn!
+        📢🎁 Chương trình khuyến mãi đặc biệt này dành cho tất cả khách hàng, bất kể bạn muốn mua bất cứ sản phẩm nào từ <b>An Khang</b>. Chỉ cần đặt hàng từ 1 sản phẩm trở lên, bạn sẽ có cơ hội tham gia quay trúng thưởng và nhận ngay những phần quà vô cùng hấp dẫn!
         <br />
         <br />
-        🎁🌟 Hãy tưởng tượng một chút, bạn có thể nhận được những phần quà gì khi tham gia chương trình này?
+        ⏰🎉 Thời gian áp dụng của chương trình kéo dài từ ngày <b>14/7</b> đến <b>31/7</b>. Đừng để lỡ cơ hội hiếm có này!
         <br />
         <br />
-        🎟️✨ Vé sức khỏe để chăm sóc tình trạng sức khoẻ của bạn, hộp tri thức để khám phá thêm những điều thú vị trong cuộc sống hoặc sẽ là hộp bí mật có chứa giữ những phần quà đầy mê hoặc.... Đó chỉ là một phần nhỏ trong vô vàn những phần quà hấp dẫn đang chờ đón bạn.
+        🎉🎁 Hãy tham gia ngay <b>&quot;Thịnh vượng cùng An Khang&quot;</b> và cùng chúng tôi chinh phục những phần quà hấp dẫn!
         <br />
         <br />
-        ⏰🎉 Thời gian áp dụng của chương trình kéo dài từ ngày 12/7 đến 31/7. Đừng để lỡ cơ hội hiếm có này!
-        <br />
-        <br />
-        🎉🎁 Hãy tham gia ngay <b>&quot;Thịnh Vượng Cùng An Khang&quot;</b> và cùng chúng tôi chinh phục những phần quà hấp dẫn!
-        <br />
-        <br />
-        <Box sx={{ wordBreak: 'break-word' }}>
+
+        <Stack flexWrap="wrap" direction="row">
           {HasTag("#AnKhangGroup")}
           {HasTag("#ThịnhVượngCùngAnKhang")}
           {HasTag("#MuaLàCóQuayLàTrúng")}
           {HasTag("#KhuyếnMãi")}
           {HasTag("#RinhQuà")}
           {HasTag("#HấpDẫn")}
-        </Box>
+        </Stack>
       </Box>
       <Box sx={{ textAlign: 'center', mt: 4 }}>
         <Button variant="contained">Vào vòng quay may mắn</Button>
